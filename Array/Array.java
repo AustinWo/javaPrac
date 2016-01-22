560c8c882d762d1f335dce27c4d3e8200d20ed69
Austin Wo <wworachet@gmail.com>
2016-01-22 11:56:50 -0800

    Forloop and Enhanced Forloop

--- Array/Array.java
+++ Array/Array.java
@@ -1,8 +1,13 @@
 class Array{
     public static void main(String[] args){
         String names[] = {"Austin", "Steven", "Jeff"};
+        // for loop
         for (int i = 0; i < names.length; i++){
             System.out.println(names[i]);
+        }
+        // enhanced for loop (foreach for loop)
+        for (String name: names){
+            System.out.println(name);
         }
     }
 }
