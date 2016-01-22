8ebf34eab2e367432278a39c373bc3fa7c190c36
Austin Wo <wworachet@gmail.com>
2016-01-21 16:04:32 -0800

    Comparing String literal and String object

---
+++ String/Str.java
@@ -0,0 +1,8 @@
+class Str{
+    public static void main(String[] args){
+        String str1 = new String("a");
+        String str2 = new String("a");
+        System.out.println(str1 == str2); // false
+        System.out.println("a" == "a"); // true
+    }
+}
