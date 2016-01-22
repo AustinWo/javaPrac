cacc8fc6c08041f3fd0eef243eb21c2dc9818587
Austin Wo <wworachet@gmail.com>
2016-01-22 01:09:58 -0800

    for loop for array

--- Array/Array.java
+++ Array/Array.java
@@ -1,5 +1,8 @@
 class Array{
     public static void main(String[] args){
         String names[] = {"Austin", "Steven", "Jeff"};
+        for (int i = 0; i < names.length; i++){
+            System.out.println(names[i]);
+        }
     }
 }
