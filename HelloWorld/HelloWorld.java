1689fbb1c6016f892dbd5a1afa13716723420fab
Austin Wo <wworachet@gmail.com>
2016-01-21 16:05:17 -0800

    Math operator and simple for loop

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -1,5 +1,16 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        // Math operator
+        double roundedNum = Math.ceil(55.55);
+        System.out.println(roundedNum);
+
+
+        // Simple For-loop
+        for (int i = 0; i < 10; i++){
+            System.out.println(i);
+        }
     }
+
 }
