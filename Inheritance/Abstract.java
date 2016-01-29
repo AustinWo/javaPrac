478f42ee3314c4d599b8bad5a6a13c11e5fdf0d3
Austin Wo <wworachet@gmail.com>
2016-01-28 21:35:32 -0800

    Implementing abstract class

--- Inheritance/Abstract.java
+++ Inheritance/Abstract.java
@@ -13,3 +13,18 @@ abstract class Fish extends Animal {
     // if the subclass doesnt implement (override the method), the method must be mark abstract
     abstract void sayType();
 }
+
+class Salmon extends Animal {
+    void sayType(){
+        System.out.println("I am a salmon");
+    }
+}
+
+class Abstract {
+    public static void main(String[] args){
+        Bird twitter = new Bird();
+        Salmon nemo = new Salmon();
+        twitter.sayType();
+        nemo.sayType();
+    }
+}
