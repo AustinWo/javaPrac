d9ab82c891aeeed044076d64f54ffdff6cbf47f7
Austin Wo <wworachet@gmail.com>
2016-01-30 09:50:38 -0800

    Sumargs

---
+++ Varargs/SumArgs.java
@@ -0,0 +1,12 @@
+class SumArgs {
+    static int sum(int ...numbers){
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++){
+            sum+= numbers[i];
+        }
+        return sum;
+    }
+    public static void main(String[] args){
+        System.out.println(SumArgs.sum(1,2,3,4,5));
+    }
+}
