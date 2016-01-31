4e7135e9fce98cd76f4f8e89c0a5193f10a9442f
Austin Wo <wworachet@gmail.com>
2016-01-31 15:00:43 -0800

    int number casting (int)

--- Number/Casting.java
+++ Number/Casting.java
@@ -2,5 +2,7 @@ class Casting{
     public static void main(String[] args){
         // Math.random() return a double
         double ranNum = Math.random();
+        // cast a number into an integer
+        int ranNumInteger = (int) ranNum;
     }
 }
