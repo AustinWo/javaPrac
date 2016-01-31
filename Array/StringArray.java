39c0871010059b3decd833094070a927272faa4c
Austin Wo <wworachet@gmail.com>
2016-01-31 14:50:06 -0800

    array literal

--- Array/StringArray.java
+++ Array/StringArray.java
@@ -1,4 +1,6 @@
 class StringArray {
     public static void main(String[] args){
+        // an array literal of type String with 3 values
+        String[] array = {"Austin", "Steven", "Jeff"};
     }
 }
