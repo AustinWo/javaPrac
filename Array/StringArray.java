bbb8976424652b0d7b08309496904e4f2ddc48b5
Austin Wo <wworachet@gmail.com>
2016-01-31 14:50:39 -0800

    cast an integer to a string

--- Array/StringArray.java
+++ Array/StringArray.java
@@ -4,5 +4,15 @@ class StringArray {
         String[] array = {"Austin", "Steven", "Jeff"};
         // an array of type String with length of 5
         String[] array2 = new String[5];
+        for (int i = 0; i < array2.length; i++){
+            // Cast an integer to a string using Integer.toString()
+            array2[i] = "Position " + Integer.toString(i+1);
+            // Cast  an integer to a string using String.valueOf()
+            // array2[i] = "Position " + String.valueOf(i+1);
+        }
+        // loop to pring the content of the String array
+        for (String content: array2){
+            System.out.println(content);
+        }
     }
 }
