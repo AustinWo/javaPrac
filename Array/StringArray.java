10fdfa3d52ce7c29c8fac0487e894e7adc653e79
Austin Wo <wworachet@gmail.com>
2016-01-31 14:50:21 -0800

    declare an array with length

--- Array/StringArray.java
+++ Array/StringArray.java
@@ -2,5 +2,7 @@ class StringArray {
     public static void main(String[] args){
         // an array literal of type String with 3 values
         String[] array = {"Austin", "Steven", "Jeff"};
+        // an array of type String with length of 5
+        String[] array2 = new String[5];
     }
 }
