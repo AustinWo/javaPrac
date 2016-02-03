69a0da021edadb47653d7496d47fa7f22e20e974
Austin Wo <wworachet@gmail.com>
2016-02-03 14:40:46 -0800

    super to call overriden method

--- Inheritance/Superclass.java
+++ Inheritance/Superclass.java
@@ -1,17 +1,33 @@
 class A {
     int firstProp = 10;
+    void makeNoise(){
+        System.out.println("from class A");
+    }
 }
 
 class B extends A {
     int secondProp = 20;
+    void makeNoise(){
+        System.out.println("from class B");
+    }
+    void makeOldNoise(){
+        super.makeNoise();
+    }
 }
 
 class Superclass{
     public static void main(String[] args){
         A objA = new B();
         B objB = new B();
-        System.out.println(objA.secondProp); // ??
+
+        A objAA = new A();
+        B objBB = new B();
+        // System.out.println(objA.secondProp); // error
         System.out.println(objB.secondProp); // ??
+
+        objAA.makeNoise();
+        objBB.makeNoise();
+        objBB.makeOldNoise();
     }
 }
 
