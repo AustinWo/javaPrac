1c224fba906ba9d1cee1f59890f99152094c1a61
Austin Wo <wworachet@gmail.com>
2016-02-04 16:26:08 -0800

    static final variable

--- Static/Static.java
+++ Static/Static.java
@@ -1,6 +1,8 @@
 class Car {
     // There is one copy of static variable for all Car objects to share
     static int currentSerialNumber = 1000;
+    // satic final variable is a constant which can be referenced without an instance of the class
+    static final int constantNum = 55;
     static int numCarsOnTheMarket;
     int serialNumber;
     // Code in static block is executed before the first stance is instantiated (only once)
@@ -30,5 +32,6 @@ class Static{
         // Static method is called wuth the class name, not an instance of the class
         Car.makeHonkNoise();
         System.out.println("There are " + Car.getNumCarsOnTheMarket() + " cars on the market.");
+        System.out.println(Car.constantNum);
     }
 }
