79078e2d19e5600a0e33e83aa891651877bb5643
Austin Wo <wworachet@gmail.com>
2016-02-07 09:34:00 -0800

    catch ArrayIndex and Arithmetic exeptions

--- Exception/ExceptionDemo.java
+++ Exception/ExceptionDemo.java
@@ -1,12 +1,16 @@
 class ExceptionDemo {
     public static void main (String[] args){
         System.out.println("before try");
-        try {
-            int[] nums = new int[5];
-            nums[6] = 7; // as soon as an error is thrown in the try block, control flow is transfer to catch block
-            System.out.println("this line won't be executed");
-        } catch (ArrayIndexOutOfBoundsException exception) {
-            System.out.println("Oops something went wrong");
+        int[] numerator = {50, 40, 30, 20, 10};
+        int[] denominator = {0, 2, 0 , 4, 0};
+        for (int i = 0; i < numerator.length+1; i++){
+            try {
+                System.out.println(Integer.toString(numerator[i]/denominator[i]));
+            } catch (ArrayIndexOutOfBoundsException exception) {
+                System.out.println("Oops that element doesn't exist");
+            } catch (ArithmeticException exception) {
+                System.out.println("cannot divide by 0");
+            }
         }
         System.out.println("resume flow");
     }
