7034e25f0a980a4558358cce47dd7ef6201e07de
Austin Wo <wworachet@gmail.com>
2016-02-07 09:20:24 -0800

    try block

--- Exception/ExceptionDemo.java
+++ Exception/ExceptionDemo.java
@@ -1,5 +1,9 @@
 class ExceptionDemo {
     public static void main (String[] args){
-
+        System.out.println("before try");
+        try {
+            int[] nums = new int[5];
+            nums[6] = 7;
+        }
     }
 }
