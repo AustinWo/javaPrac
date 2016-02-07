f6d4e16a25f7d38a0367cbb33e2b7d18dac5820d
Austin Wo <wworachet@gmail.com>
2016-02-06 18:41:29 -0800

    qualify package specification

--- Package/UseBook.java
+++ Package/UseBook.java
@@ -1,5 +1,10 @@
 class UseBook {
     public static void main(String[] args){
-        
+        bookpack.Book books[] = new bookpack.Book[2]; // qualify Book with the package name: bookpack
+        books[0] = new bookpack.Book("James", "Drawing ABC");
+        books[1] = new bookpack.Book("Susan", "Drawing ABC version II");
+        for (int i = 0; i < books.length; i++){
+            books[i].show();
+        }
     }
 }
