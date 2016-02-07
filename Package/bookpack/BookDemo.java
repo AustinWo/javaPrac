52f66991c090f0ac74a9c6f2f53d88265b3a83d2
Austin Wo <wworachet@gmail.com>
2016-02-06 18:21:01 -0800

    Book class

--- Package/bookpack/BookDemo.java
+++ Package/bookpack/BookDemo.java
@@ -1,5 +1,11 @@
 package bookpack; // this file is part of the bookpack package
 
+class Book {
+    private String author;
+    private String bookTitle;
+
+}
+
 class BookDemo {
     public static void main (String[] args){
 
