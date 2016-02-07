6ce784f55b9687966c9f878d07c5d2b4a47de713
Austin Wo <wworachet@gmail.com>
2016-02-07 09:22:42 -0800

    try transfer execution to catch

--- Exception/ExceptionDemo.java
+++ Exception/ExceptionDemo.java
@@ -4,6 +4,7 @@ class ExceptionDemo {
         try {
             int[] nums = new int[5];
             nums[6] = 7;
+            System.out.prinln("this line won't be executed")
         } catch (ArrayIndexOutOfBoundsException exception) {
             System.out.println("Oops something went wrong");
         }
