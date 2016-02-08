fa7ac40e00203a7abc7256f26483665fe7706e42
Austin Wo <wworachet@gmail.com>
2016-02-08 00:08:58 -0800

    Greeting Interface with default method

--- Interface/InterfaceDemo.java
+++ Interface/InterfaceDemo.java
@@ -1,4 +1,9 @@
 interface Greetings {
+    public void sayHello();
+    // default interface method only valid with JDK8
+    default void sayBye(){
+        System.out.println("default bye bye");
+    }
 }
 
 class Person implements Greetings {
