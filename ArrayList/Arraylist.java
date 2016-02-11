24eee958ec33e65f6c6d17e9f2a50ac0fc7c6722
Austin Wo <wworachet@gmail.com>
2016-02-10 22:44:03 -0800

    import ArrayList and Arrays

--- ArrayList/Arraylist.java
+++ ArrayList/Arraylist.java
@@ -1,5 +1,10 @@
 // import all of util 
 // import java.util.*;
+
+// import only ArrayList and Arrays
+import java.util.ArrayList;
+import java.util.Arrays;
+
 class Arraylist {
     public static void main(String[] args){
 }
