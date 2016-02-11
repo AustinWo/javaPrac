74d7244ae7b3a5b4c0b6508a8602c7bb551a5f1b
Austin Wo <wworachet@gmail.com>
2016-02-10 22:44:13 -0800

    an instance of ArrayList

--- ArrayList/Arraylist.java
+++ ArrayList/Arraylist.java
@@ -7,4 +7,9 @@ import java.util.Arrays;
 
 class Arraylist {
     public static void main(String[] args){
+        ArrayList<String> names = new ArrayList<String>();
+        names.add("Austin");
+        names.add("Steven");
+        names.add("Mark");
+        System.out.println(names); // [Austin, Steven, Mark]
 }
