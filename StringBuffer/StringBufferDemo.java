c75249240f73c9ff68fd3176ba72249ee58178f2
Austin Wo <wworachet@gmail.com>
2016-02-18 11:24:29 -0800

    StringBuffer.append()

--- StringBuffer/StringBufferDemo.java
+++ StringBuffer/StringBufferDemo.java
@@ -3,5 +3,6 @@ import java.lang.StringBuffer;
 class StringBufferDemo {
     public static void main(String[] args){
         StringBuffer myBuffer = new StringBuffer();
+        myBuffer.append("My name is Austin");
     }
 }
