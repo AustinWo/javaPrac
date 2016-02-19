5d1480bedcbc2b9be4551ee291d7f5e327546a5f
Austin Wo <wworachet@gmail.com>
2016-02-19 12:51:27 -0800

    comment

--- StringBuffer/StringBufferDemo.java
+++ StringBuffer/StringBufferDemo.java
@@ -3,8 +3,10 @@ import java.lang.StringBuffer;
 class StringBufferDemo {
     public static void main(String[] args){
         StringBuffer myBuffer = new StringBuffer();
+        // .append()
         myBuffer.append("My name is Austin");
         System.out.println(myBuffer);
+        // .reverse()
         myBuffer.reverse();
         System.out.println(myBuffer);
     }
