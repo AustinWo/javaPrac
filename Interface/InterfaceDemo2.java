520f6ff4d8576bf112395e4a3785c0b2fee38da1
Austin Wo <wworachet@gmail.com>
2016-03-05 07:38:08 -0800

    implements Athlete

--- Interface/InterfaceDemo2.java
+++ Interface/InterfaceDemo2.java
@@ -1,10 +1,16 @@
 class InterfaceDemo2 {
     public static void main(String args[]){
-
     }
 }
 
 class Student implements Athlete {
+    int speed;
+    Student(int initialSpeed) {
+        speed = initialSpeed;
+    }
+    public void run(){
+        System.out.println("I am running at " + speed + " miles per hour.");
+    }
 }
 
 interface Athlete {
